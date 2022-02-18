a24598bdb877f88373651b64a390c4b8da10cf74
HvarZ <hvarzahar@gmail.com>
2022-02-18 13:41:21 +0300

    fill matrix for next using with full set variables

--- src/com/RainCarnation/Main.java
+++ src/com/RainCarnation/Main.java
@@ -3,6 +3,24 @@ package com.RainCarnation;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello, World!");
+	    boolean[][] matrix = new boolean[16][4];
+        int levelCounter = 0;
+        int levelController = 8;
+        boolean setter = false;
+
+        for (int i = 0; i < 4; ++i) {
+            for (int j = 0; j < 16; ++j) {
+                if (levelController == levelCounter) {
+                    setter ^= true;
+                    levelCounter = 0;
+                }
+                matrix[j][i] = setter;
+                levelCounter++;
+            }
+            levelController >>= 1;
+            levelCounter = 0;
+            setter = false;
+        }
+        System.out.print(matrix[0][0]);
     }
 }

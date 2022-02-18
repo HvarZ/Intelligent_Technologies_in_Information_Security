7d9131756011a4d92bd36c289f20bca32988247b
HvarZ <hvarzahar@gmail.com>
2022-02-18 14:49:31 +0300

    delete field with variables

--- src/com/RainCarnation/NeuralNetwork.java
+++ src/com/RainCarnation/NeuralNetwork.java
@@ -4,7 +4,6 @@ package com.RainCarnation;
 // R - type for result data
 public abstract class NeuralNetwork<T, R> {
     protected float[] weights;
-    protected T[] variables;
 
     public abstract void fit(T[][] matrix, R[] result) throws NeuralException;
     public abstract R getResult(T[] input) throws NeuralException;

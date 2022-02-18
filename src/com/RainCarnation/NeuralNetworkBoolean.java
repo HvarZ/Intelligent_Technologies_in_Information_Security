0b9ba47d2cd7b8ec04ffaa1a36a782d623d72f8d
HvarZ <hvarzahar@gmail.com>
2022-02-18 14:49:15 +0300

    add service methods

--- src/com/RainCarnation/NeuralNetworkBoolean.java
+++ src/com/RainCarnation/NeuralNetworkBoolean.java
@@ -1,9 +1,19 @@
 package com.RainCarnation;
 
 public class NeuralNetworkBoolean extends NeuralNetwork<Boolean, Boolean>  {
-    public NeuralNetworkBoolean() {
+    private float trainingNorm;
+    private float probabilityError;
+
+    public NeuralNetworkBoolean(float trainingNorm_, float probabilityError_) throws NeuralException {
+        if (trainingNorm_ > 1 || trainingNorm_ < 0) {
+            throw new NeuralException("Invalid training coefficient");
+        }
+        if (probabilityError_ >= 1 || trainingNorm_ <= 0) {
+            throw new NeuralException("Invalid probability");
+        }
+        trainingNorm = trainingNorm_;
+        probabilityError = probabilityError_;
         weights = null;
-        variables = null;
     }
 
     @Override
@@ -11,12 +21,31 @@ public class NeuralNetworkBoolean extends NeuralNetwork<Boolean, Boolean>  {
         if (matrix.length <= 0 || matrix[0].length <= 0) {
             throw new NeuralException("Input data is clear");
         }
-        weights = new float[matrix[0].length];
-        variables = new Boolean[matrix[0].length];
+        if (matrix.length != result.length) {
+            throw  new NeuralException("Invalid input data");
+        }
+
+        weights = new float[matrix[0].length + 1];
     }
 
     @Override
     public Boolean getResult(Boolean[] input) {
         return true;
+    }
+
+    private int net(Boolean[] variables) {
+        int result = 0;
+        for (int i = 0; i < variables.length; ++i) {
+            result += (variables[i] ? 1 : 0) * weights[i];
+        }
+        return result;
+    }
+
+    private int fNet(int net) {
+        return net <= 0 ? 0 : 1;
+    }
+
+    private int delta(int t, int y) {
+        return t - y;
     }
 }

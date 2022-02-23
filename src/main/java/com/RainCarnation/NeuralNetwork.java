87a434bd21b12e6a751895a6a9b46b3b358c6e68
HvarZ <hvarzahar@gmail.com>
2022-02-23 15:48:01 +0300

    add new methods for saving useful information about fit

--- src/main/java/com/RainCarnation/NeuralNetwork.java
+++ src/main/java/com/RainCarnation/NeuralNetwork.java
@@ -1,6 +1,7 @@
 package com.RainCarnation;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 // T - type for input data (test or fit data)
 // R - type for result data
@@ -10,5 +11,8 @@ public abstract class NeuralNetwork<T, R> {
     public abstract void fit(T[][] matrix, R[] result) throws Exception;
     public abstract R getResult(T[] input) throws NeuralException;
     public abstract void showFit() throws IOException;
-    public abstract void showFitGraphics();
+    public abstract void showFitGraphics() throws Exception;
+    public abstract String fitResultToString();
+    public abstract void saveFitResult();
+    public abstract void saveFitResult(OutputStream out) throws IOException;
 }

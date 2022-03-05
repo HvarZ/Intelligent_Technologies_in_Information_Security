724d9096270a8ff259b582f70ee4e4c21739123e
HvarZ <hvarzahar@gmail.com>
2022-03-05 23:23:40 +0300

    add "interface' for new neural network

---
+++ src/main/java/com/RainCarnation/NeuralNetworkExtrapolation.java
@@ -0,0 +1,18 @@
+package com.RainCarnation;
+
+public class NeuralNetworkExtrapolation extends NeuralNetwork<Float, Float> {
+    @Override
+    public void fit(Float[] matrix, Float[] result) throws Exception {
+
+    }
+
+    @Override
+    public Float getResult(Float input) throws NeuralException {
+        return null;
+    }
+
+    @Override
+    public void showFitGraphics() throws Exception {
+
+    }
+}

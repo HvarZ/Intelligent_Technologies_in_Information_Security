e77c8451264567145cefc5db09361669973d960e
HvarZ <hvarzahar@gmail.com>
2022-04-08 12:12:09 +0300

    refactor float -> double

--- src/main/java/com/RainCarnation/NeuralNetworkExtrapolation.java
+++ src/main/java/com/RainCarnation/NeuralNetworkExtrapolation.java
@@ -11,16 +11,16 @@ import java.util.LinkedList;
 import java.util.Arrays;
 
 
-public class NeuralNetworkExtrapolation extends NeuralNetwork<Float, Float> {
-    private float trainingNorm;
+public class NeuralNetworkExtrapolation extends NeuralNetwork<Double, Double> {
+    private double trainingNorm;
     private int lengthWindow;
-    private float accuracy;
-    private ArrayList<Float> plotResult;
-    private Float[] lastWindow;
-    private Float[] lastWindowX;
+    private double accuracy;
+    private ArrayList<Double> plotResult;
+    private Double[] lastWindow;
+    private Double[] lastWindowX;
     private Plot plot;
 
-    private void construct(float trainingNorm_, int lengthWindow_, float accuracy_, OutputStream out) throws NeuralException {
+    private void construct(double trainingNorm_, int lengthWindow_, double accuracy_, OutputStream out) throws NeuralException {
         if (trainingNorm_ > 1 || trainingNorm_ < 0) {
             throw new NeuralException("Invalid training coefficient");
         }
@@ -39,34 +39,41 @@ public class NeuralNetworkExtrapolation extends NeuralNetwork<Float, Float> {
         plot = Plot.create();
     }
 
-    public NeuralNetworkExtrapolation(float trainingNorm_, int lengthWindow_, float accuracy_, OutputStream out) throws NeuralException {
+    public NeuralNetworkExtrapolation(double trainingNorm_, int lengthWindow_, double accuracy_, OutputStream out) throws NeuralException {
         construct(trainingNorm_, lengthWindow_, accuracy_, out);
     }
 
-    public NeuralNetworkExtrapolation(float trainingNorm_, int lengthWindow_, float accuracy_) throws NeuralException {
+    public NeuralNetworkExtrapolation(double trainingNorm_, int lengthWindow_, double accuracy_) throws NeuralException {
         construct(trainingNorm_, lengthWindow_, accuracy_, System.out);
     }
 
     @Override
-    public void fit(Float[] matrix, Float[] result) throws Exception {
+    public void fit(Double[] matrix, Double[] result) throws Exception {
         if (matrix.length <= 0) {
-            throw new NeuralException("Input data is clear");
+            throw new NeuralException("Fit: Input data is clear");
         }
 
         if (matrix.length != result.length) {
-            throw new NeuralException("Invalid input data");
+            throw new NeuralException("Fit: Invalid input data");
         }
 
         if (matrix.length <= lengthWindow) {
-            throw new NeuralException("Invalid input data");
+            throw new NeuralException("Fit: Invalid input data");
         }
 
         lastWindow = Arrays.copyOfRange(result, result.length - lengthWindow, result.length);
         lastWindowX = Arrays.copyOfRange(matrix, matrix.length - lengthWindow, matrix.length);
 
-        weights = new float[lengthWindow + 1];
+        weights = new double[lengthWindow + 1];
+        double sum = 0;
 
-        float net, epsilon, delta;
+        for (var res : result) {
+            sum += res;
+        }
+
+        //weights[0] = sum / result.length;
+
+        double net, epsilon, delta;
         int era = 0;
 
         resultWriter.write("################   Neural Network Training   ################\n");
@@ -74,36 +81,34 @@ public class NeuralNetworkExtrapolation extends NeuralNetwork<Float, Float> {
             epsilon = 0;
             era++;
             resultWriter.write("Era #" + era);
-            for (int i = 0; i < result.length - weights.length; ++i) {
+            for (int i = 0; i < result.length - lengthWindow; ++i) {
                 net = net(result, i);
-                delta = result[weights.length + i] - net;
+                delta = result[lengthWindow + i] - net;
+                epsilon += (delta * delta);
+                //weights[0] += getCorrectionWeight(delta, 1);
                 for (int j = 1; j < weights.length; ++j) {
                     weights[j] += getCorrectionWeight(delta, result[i + j - 1]);
                 }
             }
 
-            for (int i = 0; i < result.length - weights.length; ++i) {
-                net = net(result, i);
-                delta = result[weights.length + i] - net;
-                epsilon += (delta * delta);
-            }
+
             plotResult.add(epsilon);
             resultWriter.write("       Weight Coefficients: (");
 
-            for (int i = 0; i < weights.length - 1; ++i) {
+            for (int i = 0; i < lengthWindow; ++i) {
                 resultWriter.write(weights[i] + ", ");
             }
 
-            resultWriter.write(weights[weights.length - 1] + ")\n");
+            resultWriter.write(weights[lengthWindow] + ")\n");
 
-        } while ((float)Math.sqrt(epsilon) > accuracy);
+        } while (Math.sqrt(epsilon) > accuracy);
         System.out.println(era);
     }
 
     @Override
-    public Float getResult(Float input) throws NeuralException {
-        float net = 0;
-        LinkedList<Float> windowList = new LinkedList<>();
+    public Double getResult(Double input) throws NeuralException {
+        double net = 0;
+        LinkedList<Double> windowList = new LinkedList<>();
         Collections.addAll(windowList, lastWindow);
         for (int i = 0; i < getTurnToResult(input); ++i) {
             net = netList(windowList);
@@ -113,16 +118,16 @@ public class NeuralNetworkExtrapolation extends NeuralNetwork<Float, Float> {
         return net;
     }
 
-    public void addGraphicToPlot(Float[] X, Float[] Y) {
+    public void addGraphicToPlot(Double[] X, Double[] Y) {
         plot.plot().add(Arrays.asList(X), Arrays.asList(Y));
     }
 
-    public void showExtrapolation(Float finishPlot) throws Exception {
-        float net;
-        final float turn = lastWindowX[lastWindowX.length - 1] - lastWindowX[lastWindowX.length - 2];
-        LinkedList<Float> windowList = new LinkedList<>();
-        ArrayList<Float> X = new ArrayList<>();
-        ArrayList<Float> Y = new ArrayList<>();
+    public void showExtrapolation(double finishPlot) throws Exception {
+        double net;
+        final double turn = lastWindowX[lastWindowX.length - 1] - lastWindowX[lastWindowX.length - 2];
+        LinkedList<Double> windowList = new LinkedList<>();
+        ArrayList<Double> X = new ArrayList<>();
+        ArrayList<Double> Y = new ArrayList<>();
         X.add(lastWindowX[lastWindowX.length - 1]);
         Y.add(lastWindow[lastWindow.length - 1]);
 
@@ -146,16 +151,16 @@ public class NeuralNetworkExtrapolation extends NeuralNetwork<Float, Float> {
         plot.show();
     }
 
-    private float net(Float[] variables, int windowStart) {
-        float result = 0;
+    private double net(Double[] variables, int windowStart) {
+        double result = 0;
         for (int i = 1; i < weights.length; ++i) {
-            result += (weights[i] * variables[windowStart + i]);
+            result += (weights[i] * variables[windowStart + i - 1]);
         }
         return result + weights[0];
     }
 
-    private float netList(LinkedList<Float> variables) {
-        float result = 0;
+    private double netList(LinkedList<Double> variables) {
+        double result = 0;
         for (int i = 1; i < weights.length; ++i) {
             result += (weights[i] * variables.get(i - 1));
         }
@@ -163,15 +168,15 @@ public class NeuralNetworkExtrapolation extends NeuralNetwork<Float, Float> {
         return result + weights[0];
     }
 
-    private float getCorrectionWeight(float delta, float variable) {
+    private double getCorrectionWeight(double delta, double variable) {
         return trainingNorm * delta * variable;
     }
 
-    private int getTurnToResult(float input) throws NeuralException {
+    private int getTurnToResult(double input) throws NeuralException {
         if (input < lastWindowX[lastWindowX.length - 1]) {
-            throw new NeuralException("Invalid input");
+            throw new NeuralException("GetTurnToResult: Invalid input");
         }
-        float turn = lastWindowX[lastWindowX.length - 1] - lastWindowX[lastWindowX.length - 2];
+        double turn = lastWindowX[lastWindowX.length - 1] - lastWindowX[lastWindowX.length - 2];
 
         return (int)((input - lastWindowX[lastWindowX.length - 1]) / turn);
     }

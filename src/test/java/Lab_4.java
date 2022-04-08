8c262bfd856f64929e20536ccc155327f7b466f1
HvarZ <hvarzahar@gmail.com>
2022-04-08 22:16:09 +0300

    add test for lab_4

--- src/test/java/Lab_4.java
+++ src/test/java/Lab_4.java
@@ -1,9 +1,18 @@
 import com.RainCarnation.NeuralNetworkNJM;
+import java.io.FileOutputStream;
 
 public class Lab_4 {
     public static void main(String[] args) throws Exception {
-        NeuralNetworkNJM network = new NeuralNetworkNJM(1, "2-1-2");
+        FileOutputStream out = new FileOutputStream("results/lab_4.txt");
 
-        network.fit(new Double[]{1d, 1d, 2d}, new Double[]{2d, 2d});
+        NeuralNetworkNJM network = new NeuralNetworkNJM(1, "2-1-2", out);
+        Double[][] testData = new Double[][]{{1d, 1d, 2d}};
+
+        network.fit(testData, new Double[]{0.2, 0.2});
+        Double[] result = network.getResult(new Double[]{1d, 1d, 2d});
+
+        System.out.println(result[0] + " " + result[1]);
+        network.showFitGraphics();
+        network.showFit();
     }
 }

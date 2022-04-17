da2d5a8f518d3db4e5359fdb6d90708176165a89
HvarZ <hvarzahar@gmail.com>
2022-04-17 17:44:34 +0300

    finish test

--- src/test/java/Lab_3.java
+++ src/test/java/Lab_3.java
@@ -4,10 +4,6 @@ import com.RainCarnation.NeuralNetworkBooleanRBF;
 public class Lab_3 {
     public static void main(String[] args) throws Exception {
         Boolean[][] matrixFull = NeuralNetworkBoolean.getStandardMatrix(4);
-        Boolean[] resultFull = {false, true, false, true,
-                false, true, true, true,
-                false, true, true, true,
-                false, true, true, true};
 
         Boolean[] resultBooleanFunction = {false, true, false, true,
                 false, true, true, true,
@@ -18,11 +14,16 @@ public class Lab_3 {
                 {false, false, true, false},
                 {false, true, true, false},
                 {true, false, true, false},
-                {true, true, false, false}};
+                {true, true, false, false},
+                {true, true, true, true}};
 
-        Boolean[] resultPart = {true, false, true, true, false};
+        Boolean[] result = {true, false, true, true, false, true};
 
         NeuralNetworkBoolean network = new NeuralNetworkBooleanRBF(0.3 , resultBooleanFunction);
-        network.fit(matrix, resultPart);
+        network.fit(matrix, result);
+
+        for (Boolean[] standardVector : matrixFull) {
+            System.out.println(network.getResult(standardVector));
+        }
     }
 }

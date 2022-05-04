f12a3056683ffeba85eff2f56c464c9cfe8e0d29
HvarZ <hvarzahar@gmail.com>
2022-05-04 20:52:38 +0300

    Add DI for laboratory work #2

--- src/test/java/ConfigurationLab.java
+++ src/test/java/ConfigurationLab.java
@@ -4,16 +4,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Scope;
 
 import java.io.FileOutputStream;
 
 @Configuration
 @PropertySource("classpath:Labs.properties")
 public class ConfigurationLab {
+    // FirstLab
     @Value("${numberVariables}")
     int numberVariables;
     @Value("${filenameResult}")
-    private String filename;
+    private String filename_1;
 
     @Value("${trainingNorm}")
     private Double trainingNorm;
@@ -32,7 +34,7 @@ public class ConfigurationLab {
 
     @Bean
     public NeuralNetworkBoolean thresholdFull() throws Exception {
-        out = new FileOutputStream(filename, true);
+        out = new FileOutputStream(filename_1, true);
         NeuralNetworkBoolean network = new NeuralNetworkBooleanThreshold(trainingNorm, out);
         Boolean[][] matrixFull = NeuralNetworkBoolean.getStandardMatrix(numberVariables);
         network.fit(matrixFull, NeuralNetworkBoolean.convertStringVectorToArray(resultFull));
@@ -42,7 +44,7 @@ public class ConfigurationLab {
 
     @Bean
     public NeuralNetworkBoolean thresholdPart() throws Exception {
-        out = new FileOutputStream(filename, true);
+        out = new FileOutputStream(filename_1, true);
         NeuralNetworkBoolean network = new NeuralNetworkBooleanThreshold(trainingNorm, out);
         network.fit(NeuralNetworkBoolean.convertStringVectorsToMatrix(matrixPart), NeuralNetworkBoolean.convertStringVectorToArray(resultPart));
 
@@ -51,7 +53,7 @@ public class ConfigurationLab {
 
     @Bean
     public NeuralNetworkBoolean logisticFull() throws Exception {
-        out = new FileOutputStream(filename, true);
+        out = new FileOutputStream(filename_1, true);
         NeuralNetworkBoolean network = new NeuralNetworkBooleanLogistic(trainingNorm, out);
         Boolean[][] matrixFull = NeuralNetworkBoolean.getStandardMatrix(numberVariables);
         network.fit(matrixFull, NeuralNetworkBoolean.convertStringVectorToArray(resultFull));
@@ -61,11 +63,58 @@ public class ConfigurationLab {
 
 
     @Bean
+    @Scope("prototype")
     public NeuralNetworkBoolean logisticPart() throws Exception {
-        out = new FileOutputStream(filename, true);
+        out = new FileOutputStream(filename_1, true);
         NeuralNetworkBoolean network = new NeuralNetworkBooleanLogistic(trainingNorm, out);
         network.fit(NeuralNetworkBoolean.convertStringVectorsToMatrix(matrixPart), NeuralNetworkBoolean.convertStringVectorToArray(resultPart));
 
         return network;
     }
+
+    // Second Lab
+
+    @Value("${a_1}")
+    private Double a_1;
+
+    @Value("${a_2}")
+    private Double a_2;
+
+    @Value("${b_1}")
+    private Double b_1;
+
+    @Value("${b_2}")
+    private Double b_2;
+
+    @Value("${numberPoints}")
+    private int numberPoints;
+
+
+    @Value("${filename_2}")
+    private String filename_2;
+
+    @Value("${trainingNorm_2}")
+    private Double trainingNorm_2;
+
+    @Value("${lengthWindow_2}")
+    private int lengthWindow_2;
+
+    @Value("${accuracy_2}")
+    private Double accuracy_2;
+
+    @Bean
+    @Scope("prototype")
+    public NeuralNetworkExtrapolation extrapolation() throws Exception {
+        out = new FileOutputStream(filename_2, true);
+        Double[][] discreteValues = NeuralNetworkExtrapolation.generateDiscreteFunction(a_1, b_1, numberPoints, Math::tan);
+        Double[][] discreteValues_2 = NeuralNetworkExtrapolation.generateDiscreteFunction(a_2, b_2, numberPoints, Math::tan);
+
+        NeuralNetworkExtrapolation network = new NeuralNetworkExtrapolation(trainingNorm_2, lengthWindow_2, accuracy_2, out);
+        network.fit(discreteValues[0], discreteValues[1]);
+        network.addGraphicToPlot(discreteValues[0], discreteValues[1]);
+        network.addGraphicToPlot(discreteValues_2[0], discreteValues_2[1]);
+
+        return network;
+    }
+
 }
--- src/test/java/Lab_2.java
+++ src/test/java/Lab_2.java
@@ -1,48 +1,13 @@
 import com.RainCarnation.NeuralNetworkExtrapolation;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
 
 public class Lab_2 {
-    @FunctionalInterface
-    public interface Function {
-        double result(double i);
-    }
+    public static void main(String[] args) throws Exception {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ConfigurationLab.class);
+        NeuralNetworkExtrapolation network = context.getBean("extrapolation", NeuralNetworkExtrapolation.class);
 
-    private static Double[][] generateDiscreteFunction(double a, double b, int number, Function function) {
-        Double[][] result = new Double[2][number];
-        double turn = (b - a) / (number + 1);
-
-        for (int i = 1; i < number + 1; ++i) {
-            result[0][i - 1] = a + i * turn;
-            result[1][i - 1] = function.result(a + i * turn);
-        }
-        return result;
-    }
-
-
-    public static void main(String[] args) {
-        Double[][] discreteValues = generateDiscreteFunction(2, 3, 20, Math::tan);
-        Double[][] discreteValues_2 = generateDiscreteFunction(3, 4, 20, Math::tan);
-
-        FileOutputStream out = null;
-        try {
-            out = new FileOutputStream("results/lab_2.txt", true);
-            new FileWriter("results/lab_2.txt", false).close();
-            NeuralNetworkExtrapolation network = new NeuralNetworkExtrapolation(0.1f, 6, 0.001f, out);
-            network.fit(discreteValues[0], discreteValues[1]);
-            network.addGraphicToPlot(discreteValues[0], discreteValues[1]);
-            network.addGraphicToPlot(discreteValues_2[0], discreteValues_2[1]);
-            network.showExtrapolation(4);
-        } catch (Exception exception) {
-            System.err.println(exception.getMessage());
-        } finally {
-            try {
-                out.close();
-            } catch (IOException | NullPointerException e) {
-                System.err.println("Invalid closing");
-            }
-        }
+        network.showExtrapolation(4);
+        context.close();
     }
 }

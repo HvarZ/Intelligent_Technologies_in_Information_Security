d11790e254da8e54244b7e57e33c6d77902e9127
HvarZ <hvarzahar@gmail.com>
2022-05-04 21:46:40 +0300

    clearing file since start

--- src/test/java/Lab_2.java
+++ src/test/java/Lab_2.java
@@ -1,9 +1,12 @@
 import com.RainCarnation.NeuralNetworkExtrapolation;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.io.FileWriter;
+
 
 public class Lab_2 {
     public static void main(String[] args) throws Exception {
+        new FileWriter("results/lab_2.txt", false).close();
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ConfigurationLab.class);
         NeuralNetworkExtrapolation network = context.getBean("extrapolation", NeuralNetworkExtrapolation.class);
 

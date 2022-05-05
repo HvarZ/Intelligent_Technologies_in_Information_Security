569f138f41226dcfedee877e4a6e5ced9dce6d00
HvarZ <hvarzahar@gmail.com>
2022-05-05 14:21:59 +0300

    add testing function in Lab_3 test file

--- src/test/java/Lab_3.java
+++ src/test/java/Lab_3.java
@@ -4,20 +4,16 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import java.io.FileWriter;
 
 public class Lab_3 {
+    private static void testing(AnnotationConfigApplicationContext context, String beanId, String report) throws Exception {
+        NeuralNetworkBoolean network = context.getBean(beanId, NeuralNetworkBoolean.class);
+        network.showFit(report);
+        network.showFitGraphics();
+    }
     public static void main(String[] args) throws Exception {
         new FileWriter("results/lab_3.txt", false).close();
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ConfigurationLab.class);
-
-        NeuralNetworkBoolean network = context.getBean("extrapolationThreshold", NeuralNetworkBoolean.class);
-        network.showFit("============= RBF Threshold =============\n");
-        network.showFitGraphics();
-
-
-        network = context.getBean("extrapolationThreshold", NeuralNetworkBoolean.class);
-        network.showFit("============= RBF Logistic =============\n");
-        network.showFitGraphics();
-
-
+        testing(context, "extrapolationThreshold", "============= RBF Threshold =============\n");
+        testing(context, "extrapolationLogistic", "============= RBF Logistic =============\n");
         context.close();
 
     }

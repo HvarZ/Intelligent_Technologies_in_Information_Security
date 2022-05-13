d94aede47b9361951176c80a0cb3f609c9356cb1
HvarZ <hvarzahar@gmail.com>
2022-05-13 18:41:28 +0300

    add part of linear algebra in Hopfield network

--- src/main/java/com/RainCarnation/NeuralNetworkHopfield.java
+++ src/main/java/com/RainCarnation/NeuralNetworkHopfield.java
@@ -1,9 +1,27 @@
 package com.RainCarnation;
 
-public class NeuralNetworkHopfield extends NeuralNetwork<Integer[][], Integer, Integer[], Integer[]> {
-    @Override
-    public void fit(Integer[][] matrix, Integer... result) throws Exception {
+import com.RainCarnation.service.BinaryImage;
 
+public class NeuralNetworkHopfield extends NeuralNetwork<BinaryImage[], Integer, Integer[], Integer[]> {
+    private int[][] weights;
+    @Override
+    public void fit(BinaryImage[] matrix, Integer... result) throws Exception {
+
+        if (matrix.length <= 0) {
+            throw new NeuralException("Input data is clear");
+        }
+
+        int[][] vectorizedImages = new int[matrix.length][];
+
+        for (int i = 0; i < matrix.length; ++i) {
+            vectorizedImages[i] = BinaryImage.vectorize(matrix[i].getImage());
+        }
+
+        weights = new int[vectorizedImages[0].length][];
+
+        for (int i = 0; i < matrix.length; ++i) {
+            weights = sum(weights, multiple(vectorizedImages[i], vectorizedImages[i]));
+        }
     }
 
     @Override
@@ -15,4 +33,38 @@ public class NeuralNetworkHopfield extends NeuralNetwork<Integer[][], Integer, I
     public void showFitGraphics() throws Exception {
 
     }
+
+    public static int[][] multiple(int[] a, int[] b) {
+        if (a.length != b.length) {
+            throw new ArithmeticException("Invalid arguments: matrix (a, b)");
+        }
+
+        int[][] matrix = new int[a.length][b.length];
+
+        for (int i = 0; i < a.length; ++i) {
+            for (int j = 0; j < a.length; ++j) {
+                matrix[i][j] = a[i] * b[j];
+            }
+        }
+
+        return matrix;
+    }
+
+    public static int[][] sum(int[][] a, int[][] b) {
+        if (a.length != b.length || a[0].length != b[0].length) {
+            throw new ArithmeticException("Invalid arguments: matrix (a, b)");
+        }
+
+        int[][] sum = new int[a.length][a[0].length];
+
+        for (int i = 0; i < a.length; ++i) {
+            for (int j = 0; j < a[0].length; ++j) {
+                sum[i][j] = a[i][j] + b[i][j];
+            }
+        }
+
+        return sum;
+    }
+
+
 }

d8c72efa31e441afc8f530a3243095e4fb9714fc
HvarZ <hvarzahar@gmail.com>
2022-05-13 13:42:06 +0300

    add Hopfield Neural Network

---
+++ src/main/java/com/RainCarnation/NeuralNetworkHopfield.java
@@ -0,0 +1,18 @@
+package com.RainCarnation;
+
+public class NeuralNetworkHopfield extends NeuralNetwork<Integer[][], Integer, Integer[], Integer[]> {
+    @Override
+    public void fit(Integer[][] matrix, Integer... result) throws Exception {
+
+    }
+
+    @Override
+    public Integer[] getResult(Integer[] input) throws NeuralException {
+        return null;
+    }
+
+    @Override
+    public void showFitGraphics() throws Exception {
+
+    }
+}

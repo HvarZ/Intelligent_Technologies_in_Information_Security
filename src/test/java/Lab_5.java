c980484cdfcb027a63bdd255c32efce3cf68f1a1
HvarZ <hvarzahar@gmail.com>
2022-05-15 18:31:20 +0300

    add test 5

--- src/test/java/Lab_5.java
+++ src/test/java/Lab_5.java
@@ -1,5 +1,47 @@
-public class Lab_5 {
-    public static void main(String[] args) {
+import com.RainCarnation.NeuralNetworkHopfield;
+import com.RainCarnation.service.BinaryImage;
 
+public class Lab_5 {
+    public static void main(String[] args) throws Exception {
+        BinaryImage image_3 = new BinaryImage(new int[][]{
+                {+1, +1, +1, +1, +1},
+                {-1, -1, -1, -1, +1},
+                {-1, -1, -1, -1, +1},
+                {-1, +1, +1, +1, -1},
+                {-1, -1, -1, -1, +1},
+                {-1, -1, -1, -1, +1},
+                {+1, +1, +1, +1, +1}
+        });
+
+        BinaryImage image_5 = new BinaryImage(new int[][]{
+                {+1, +1, +1, +1, +1},
+                {+1, -1, -1, -1, -1},
+                {+1, -1, -1, -1, -1},
+                {+1, +1, +1, +1, +1},
+                {-1, -1, -1, -1, +1},
+                {-1, -1, -1, -1, +1},
+                {+1, +1, +1, +1, +1}
+        });
+
+        BinaryImage image_7 = new BinaryImage(new int[][]{
+                {+1, +1, +1, +1, +1},
+                {-1, -1, -1, +1, -1},
+                {-1, -1, -1, +1, -1},
+                {-1, -1, +1, -1, -1},
+                {-1, -1, +1, -1, -1},
+                {-1, +1, -1, -1, -1},
+                {-1, +1, -1, -1, -1}
+        });
+
+        NeuralNetworkHopfield network = new NeuralNetworkHopfield();
+        network.fit(new BinaryImage[]{image_3, image_5, image_7});
+
+        BinaryImage resultImage_3 = network.getResult(image_3);
+        BinaryImage resultImage_5 = network.getResult(image_5);
+        BinaryImage resultImage_7 = network.getResult(image_7);
+
+        resultImage_3.print();
+        resultImage_5.print();
+        resultImage_7.print();
     }
 }

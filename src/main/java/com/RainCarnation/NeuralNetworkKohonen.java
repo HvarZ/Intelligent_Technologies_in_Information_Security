59ba9b574fc2c043b9be7dcc065448a373a25874
HvarZ <hvarzahar@gmail.com>
2022-05-20 15:46:18 +0300

    finish network Kohonen

--- src/main/java/com/RainCarnation/NeuralNetworkKohonen.java
+++ src/main/java/com/RainCarnation/NeuralNetworkKohonen.java
@@ -2,19 +2,37 @@ package com.RainCarnation;
 
 import com.RainCarnation.service.Measurable;
 
-public class NeuralNetworkKohonen<T extends Measurable, Cluster> extends NeuralNetwork<T[], Cluster[], T, Cluster> {
-    @Override
-    public void fit(T[] matrix, Cluster[]... result) throws Exception {
 
+public class NeuralNetworkKohonen<T extends Measurable, Cluster extends Measurable> extends NeuralNetwork<T[], Cluster, T, Cluster> {
+    Cluster[] clusters;
+    @Override
+    public void fit(T[] matrix, Cluster... result) throws Exception {
+        if (matrix.length == 0 || result.length == 0) {
+            throw new NeuralException("Input data is clear");
+        }
+        clusters = result;
     }
 
     @Override
-    public Cluster getResult(T input) throws NeuralException {
-        return null;
+    public Cluster getResult(T input) {
+        double distance;
+        double distanceMin = Integer.MAX_VALUE;
+        Cluster currentCluster = null;
+        for (Cluster cluster : clusters) {
+           distance = distance(cluster.getX(), cluster.getY(), input.getX(), input.getY());
+           if (distance < distanceMin) {
+               currentCluster = cluster;
+               distanceMin = distance;
+           }
+        }
+        return currentCluster;
     }
 
     @Override
-    public void showFitGraphics() throws Exception {
+    @Deprecated
+    public void showFitGraphics() {}
 
+    private double distance(double x1, double y1, double x2, double y2) {
+        return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
     }
 }

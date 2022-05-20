9d8c1b5d6f4fd5cc4a76acabd9b711e00aeae08d
HvarZ <hvarzahar@gmail.com>
2022-05-20 12:37:45 +0300

    add converting methods

--- src/main/java/com/RainCarnation/service/BinaryImage.java
+++ src/main/java/com/RainCarnation/service/BinaryImage.java
@@ -1,5 +1,7 @@
 package com.RainCarnation.service;
 
+import com.RainCarnation.NeuralException;
+
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -50,4 +52,34 @@ public final class BinaryImage {
         writer.flush();
     }
 
+    static public int[] convertStringVectorToLine(String vector) throws NeuralException {
+        if (!vector.matches("^[0|1]*$")) {
+            throw new NeuralException("InvalidVector");
+        }
+        int[] result = new int[vector.length()];
+
+        for (int i = 0; i < result.length; ++i) {
+            result[i] = vector.charAt(i) == '1' ? 1 : -1;
+        }
+
+        return result;
+    }
+    static public BinaryImage convertStringToBinaryImage(String vector) throws NeuralException {
+        int index = vector.indexOf(','), numberTerminators = 1;
+        if (index == - 1) {
+            index = vector.length();
+        }
+        for (char ch : vector.toCharArray()) {
+            if (ch == ',') {
+                numberTerminators++;
+            }
+        }
+        String[] vectors = vector.split(",");
+        int[][] result = new int[numberTerminators][index];
+        for (int i = 0; i < result.length; ++i) {
+            result[i] = convertStringVectorToLine(vectors[i]);
+        }
+
+        return new BinaryImage(result);
+    }
 }

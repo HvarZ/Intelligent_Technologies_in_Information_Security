a174f57099e00a184396a077cd8abad08ea945b9
HvarZ <hvarzahar@gmail.com>
2022-05-20 15:45:49 +0300

    final lab_6

--- src/test/java/Lab_6.java
+++ src/test/java/Lab_6.java
@@ -1,3 +1,4 @@
+import com.RainCarnation.NeuralNetworkKohonen;
 import com.RainCarnation.service.Measurable;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -9,6 +10,34 @@ import java.io.IOException;
 import java.io.Reader;
 
 public class Lab_6 {
+    public static class District implements Measurable {
+        private final double X;
+        private final double Y;
+
+        private final String name;
+
+        public District(double x, double y, String name) {
+            this.X = x;
+            this.Y = y;
+            this.name = name;
+        }
+
+        @Override
+        public double getX() {
+            return X;
+        }
+
+        @Override
+        public double getY() {
+            return Y;
+        }
+
+        @Override
+        public String getId() {
+            return name;
+        }
+    }
+
     public static class Hospital implements Measurable {
         private final double X;
         private final double Y;
@@ -49,21 +78,21 @@ public class Lab_6 {
         try (Reader reader = new FileReader(filename)) {
             JSONArray hospitalsJ = (JSONArray) parser.parse(reader);
             hospitals = new Hospital[hospitalsJ.size()];
-            JSONObject hospital, geoData = null;
-            JSONArray coordinates, district = null;
+            JSONObject hospital, geoData;
+            JSONArray coordinates, district;
             double x, y;
             String name, area;
             for (int i = 0; i < hospitalsJ.size(); ++i) {
                 hospital = (JSONObject) hospitalsJ.get(i);
-                name = (String)hospital.get("FullName");
+                name = (String) hospital.get("FullName");
 
                 district = (JSONArray) hospital.get("ObjectAddress");
                 area = (String) ((JSONObject) district.get(0)).get("AdmArea");
 
                 geoData = (JSONObject) hospital.get("geoData");
-                coordinates = (JSONArray)((JSONArray) geoData.get("coordinates")).get(0);
-                x = (double) coordinates.get(0);
-                y = (double) coordinates.get(1);
+                coordinates = (JSONArray) ((JSONArray) geoData.get("coordinates")).get(0);
+                x = (double) coordinates.get(1);
+                y = (double) coordinates.get(0);
 
                 hospitals[i] = new Hospital(x, y, name, area);
             }
@@ -73,9 +102,50 @@ public class Lab_6 {
         return hospitals;
     }
 
-    static public void main(String[] args) throws ParseException {
-        Hospital[] hospitals = parseJsonToHospitals("src/test/resources/Hospitals.json");
+    private static District[] parseJsonToDistricts(String filename) {
+        JSONParser parser = new JSONParser();
+        District[] districts = null;
 
-        System.out.println(124);
+        try (Reader reader = new FileReader(filename)) {
+            JSONObject object = (JSONObject) parser.parse(reader);
+            JSONArray districtsJ = (JSONArray) object.get("Districts");
+            JSONObject district;
+
+            String name;
+            double x, y;
+            districts = new District[districtsJ.size()];
+            for (int i = 0; i < districts.length; ++i) {
+                district = (JSONObject) districtsJ.get(i);
+                name = (String) district.get("FullName");
+                x = (double) ((JSONArray) (district.get("Coordinates"))).get(0);
+                y = (double) ((JSONArray) (district.get("Coordinates"))).get(1);
+                districts[i] = new District(x, y, name);
+            }
+        } catch (IOException | ParseException exception) {
+            exception.printStackTrace();
+        }
+
+        return districts;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Hospital[] hospitals = parseJsonToHospitals("src/test/resources/Hospitals.json");
+        District[] districts = parseJsonToDistricts("src/test/resources/Districts.json");
+
+        NeuralNetworkKohonen<Hospital, District> network = new NeuralNetworkKohonen<>();
+        network.fit(hospitals, districts);
+
+        double counter = 0;
+        District district;
+        for (Hospital hospital: hospitals) {
+            district = network.getResult(hospital);
+            if (district.getId().equals(hospital.getArea())) {
+                System.out.print(1);
+                counter++;
+            } else {
+                System.out.print(0);
+            }
+        }
+        System.out.println(" " + (counter / hospitals.length) * 100);
     }
 }

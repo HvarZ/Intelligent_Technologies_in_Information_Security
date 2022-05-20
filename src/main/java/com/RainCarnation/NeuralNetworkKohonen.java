ee508a3c92f0594bcc72617055a2122dfff38836
HvarZ <hvarzahar@gmail.com>
2022-05-20 15:53:02 +0300

    override deprecated marks methods

--- src/main/java/com/RainCarnation/NeuralNetworkKohonen.java
+++ src/main/java/com/RainCarnation/NeuralNetworkKohonen.java
@@ -2,6 +2,7 @@ package com.RainCarnation;
 
 import com.RainCarnation.service.Measurable;
 
+import java.io.OutputStream;
 
 public class NeuralNetworkKohonen<T extends Measurable, Cluster extends Measurable> extends NeuralNetwork<T[], Cluster, T, Cluster> {
     Cluster[] clusters;
@@ -31,6 +32,21 @@ public class NeuralNetworkKohonen<T extends Measurable, Cluster extends Measurab
     @Override
     @Deprecated
     public void showFitGraphics() {}
+
+
+    @Override
+    @Deprecated
+    public String fitResultToString() {
+        return "";
+    }
+
+    @Override
+    @Deprecated
+    public void saveFitResult() {}
+
+    @Override
+    @Deprecated
+    public void saveFitResult(OutputStream out) {}
 
     private double distance(double x1, double y1, double x2, double y2) {
         return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
